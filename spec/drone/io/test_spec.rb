57db9927613b39db82db5816369639a6a4afa3b3
ajapon88 <ajapon88@gmail.com>
2018-11-13 00:19:44 +0900

    fix gemspec

--- spec/drone/io/test_spec.rb
+++ spec/drone/io/test_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Drone::Io::Test do
   it "has a version number" do
     expect(Drone::Io::Test::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
